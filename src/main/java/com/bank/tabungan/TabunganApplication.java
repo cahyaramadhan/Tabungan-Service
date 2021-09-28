4d0b9972e09b204d70b1b4931266e85e29c6ba72
Cahya Ramadhan <cahyaramadhan76@gmail.com>
2021-09-28 11:50:28 +0700

    - TabunganApplication extends SpringBootServletInitializer

--- src/main/java/com/bank/tabungan/TabunganApplication.java
+++ src/main/java/com/bank/tabungan/TabunganApplication.java
@@ -2,11 +2,12 @@ package com.bank.tabungan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class TabunganApplication {
+public class TabunganApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TabunganApplication.class, args);
