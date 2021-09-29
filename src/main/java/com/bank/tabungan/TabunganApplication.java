9c8413b05deb06d8cb3a68bd3873bffe92f3a5c9
Cahya Ramadhan <cahyaramadhan76@gmail.com>
2021-09-29 16:12:52 +0700

    - Add Error

--- src/main/java/com/bank/tabungan/TabunganApplication.java
+++ src/main/java/com/bank/tabungan/TabunganApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class TabunganApplication extends SpringBootServletInitializer {
+public class TabunganApplicationError extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TabunganApplication.class, args);
